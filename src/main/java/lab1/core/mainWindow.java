f55d3c2d480d431c7d960db71f450ab76800456d
PL97 <pengle@126.com>
2017-10-15 17:28:55 +0800

    commit C4

--- src/main/java/lab1/core/mainWindow.java
+++ src/main/java/lab1/core/mainWindow.java
@@ -63,7 +63,6 @@ public class mainWindow {
         //}
         //test random walk
         //for (int i = 0; i < 20; i++) {
-        System.out.println(randomWalk(G));
         //}
 
         //test create new text
